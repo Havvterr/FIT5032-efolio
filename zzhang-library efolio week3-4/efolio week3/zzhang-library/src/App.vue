780ab1a836e514de7bb76060014691b925048d6e
Zehui Zhang <zzha0455@student.monash.edu>
2024-08-16 12:18:22 +1000

    Finish efolio week3

---
+++ zzhang-library efolio week3-4/efolio week3/zzhang-library/src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import JSONLab from './components/JSON.vue'
+import Form from './components/Form.vue'
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <!--<img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
+    </header>
+
+    <main>
+      <!--<JSONLab />*/-->
+      <Form />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+
+main {
+  padding: 20px;
+}
+</style>
